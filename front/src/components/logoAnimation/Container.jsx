7252249e3e6e8460589fafe73a558412a56d0a47
edenx909 <edenx909@gmail.com>
2024-11-12 17:54:58 +1100

    now perpetually animates

--- front/src/components/logoAnimation/Container.jsx
+++ front/src/components/logoAnimation/Container.jsx
@@ -14,44 +14,80 @@ const Container = () => {
       >
         <Logo fill={"#black"} />
       </motion.div>
+      {/* CONTAINER */}
       <motion.div
         className="absolute"
-        initial={{ opacity: 0, filter: "blur(9px)" }}
-        animate={{
-          scale: 1.2,
-          rotate: "30deg",
-          opacity: 1,
-          filter: "blur(0px)",
+        animate={{ rotate: 360 }}
+        transition={{
+          duration: 60,
+
+          repeat: Infinity,
+          repeatType: "loop",
         }}
-        transition={{ duration: 0.5, ease: "easeOut" }}
       >
-        <Circles cirleDots={16} spread={"3rem"} size={"0.5rem"} />
+        {/* CIRCLE */}
+        <motion.div
+          initial={{ opacity: 0, filter: "blur(9px)" }}
+          animate={{
+            scale: 1.2,
+            rotate: "30deg",
+            opacity: 1,
+            filter: "blur(0px)",
+          }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+        >
+          <Circles cirleDots={16} spread={"3rem"} size={"0.5rem"} />
+        </motion.div>
       </motion.div>
+      {/* CONTAINER */}
       <motion.div
         className="absolute"
-        initial={{ opacity: 0, filter: "blur(7px)" }}
-        animate={{
-          scale: 1.2,
-          rotate: "45deg",
-          opacity: 1,
-          filter: "blur(0px)",
+        animate={{ rotate: 360 }}
+        transition={{
+          duration: 120,
+
+          repeat: Infinity,
+          repeatType: "loop",
         }}
-        transition={{ duration: 0.6, ease: "easeOut" }}
       >
-        <Circles cirleDots={17} spread={"4rem"} size={"0.6rem"} />
+        {/* CIRCLE */}
+        <motion.div
+          initial={{ opacity: 0, filter: "blur(7px)" }}
+          animate={{
+            scale: 1.2,
+            rotate: "45deg",
+            opacity: 1,
+            filter: "blur(0px)",
+          }}
+          transition={{ duration: 0.6, ease: "easeOut" }}
+        >
+          <Circles cirleDots={17} spread={"4rem"} size={"0.6rem"} />
+        </motion.div>
       </motion.div>
+      {/* CONTAINER */}
       <motion.div
         className="absolute"
-        initial={{ opacity: 0, filter: "blur(3px)" }}
-        animate={{
-          scale: 1.2,
-          rotate: "60deg",
-          opacity: 1,
-          filter: "blur(0px)",
+        animate={{ rotate: 360 }}
+        transition={{
+          duration: 180,
+
+          repeat: Infinity,
+          repeatType: "loop",
         }}
-        transition={{ duration: 0.9, ease: "easeOut" }}
       >
-        <Circles cirleDots={18} spread={"5rem"} size={"0.7rem"} />
+        {/* CIRCLE */}
+        <motion.div
+          initial={{ opacity: 0, filter: "blur(3px)" }}
+          animate={{
+            scale: 1.2,
+            rotate: "60deg",
+            opacity: 1,
+            filter: "blur(0px)",
+          }}
+          transition={{ duration: 0.9, ease: "easeOut" }}
+        >
+          <Circles cirleDots={18} spread={"5rem"} size={"0.7rem"} />
+        </motion.div>
       </motion.div>
     </div>
   );
