1e7a97b7012e2f8a674a99d1c2b4bdd5a7b18657
edenx909 <edenx909@gmail.com>
2024-11-15 18:40:33 +1100

    host before more fixes

--- back/server.js
+++ back/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -10,6 +11,8 @@ import connectToDb from "./db/connectToDb.js";
 import { app, server } from "./socket/socket.js";
 const PORT = process.env.PORT || 8000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
@@ -18,8 +21,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("server running");
+app.use(express.static(path.join(__dirname, "/front/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "front", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
